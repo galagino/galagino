b94f98f1f9a0cef358c7f87d9ad67babe5f9bcea
galagino <galagino@galagino>
2026-09-01 18:24:10 +0000

    galaga fire button fix from https://github.com/alijani1

--- source/src/machines/galaga/galaga.cpp
+++ source/src/machines/galaga/galaga.cpp
@@ -34,56 +34,56 @@ unsigned char galaga::rdZ80(unsigned short Addr) {
   if((Addr&0xfe00) == 0x7000) {
     if(Addr & 0x100) {
       return namco_busy ? 0x00 : 0x10;   // cmd ack (game_ctrl.s L1024)
-    } else  {
+    } 
+    else  {
       unsigned char retval = 0x00;
       
       if(cs_ctrl & 1) {   // bit[0] -> 51xx selected
-	      if(!credit_mode) {
-	        // galaga doesn't seem to use the button mappings in byte 1 and 2 in 
-	        // non-credit mode. So we don't implement that
-	        unsigned char map71[] = { 0b11111111, 0xff, 0xff };
-	        if(namco_cnt > 2) return 0xff;
-	  
-	        retval = map71[namco_cnt];
-	      } else {
-	        static unsigned char prev_mask = 0;
-	        static unsigned char fire_timer = 0;
+        if(!credit_mode) {
+          // galaga doesn't seem to use the button mappings in byte 1 and 2 in 
+          // non-credit mode. So we don't implement that
+          unsigned char map71[] = { 0b11111111, 0xff, 0xff };
+          if(namco_cnt > 2) return 0xff;
           
-	        // byte 0 is credit in BCD, byte 1 and 2: ...FLURD 
-	        unsigned char mapb1[] = { (unsigned char)(16*(credit/10) + credit % 10), 0b11111111, 0b11111111 };
-	  
-	        // get a mask of currently pressed keys
-	        unsigned char keymask = input->buttons_get();
-	  
-	        // report directions directly
-	        if(keymask & BUTTON_LEFT)  mapb1[1] &= ~0x08;
-	        if(keymask & BUTTON_UP)    mapb1[1] &= ~0x04;
-	        if(keymask & BUTTON_RIGHT) mapb1[1] &= ~0x02;
-	        if(keymask & BUTTON_DOWN)  mapb1[1] &= ~0x01;
-	  
-	        // report fire only when it was pressed
-	        if((keymask & BUTTON_FIRE) && !(prev_mask & BUTTON_FIRE)) {
-	          mapb1[1] &= ~0x10;
-	          fire_timer = 1;         // 0 is too short for score enter, 5 is too long
-	          // should probably be done via a global counter
-	        } else if(fire_timer) {
-	          mapb1[1] &= ~0x10;
-	          fire_timer--;
-	        }
-	  
-	        // 51xx leaves credit mode when user presses start? Nope ...
-	        if((keymask & BUTTON_START) && !(prev_mask & BUTTON_START) && credit)
-	          credit -= 1;
-	  
-	        if((keymask & BUTTON_COIN) && !(prev_mask & BUTTON_COIN) && (credit < 99))
-	          credit += 1;
-	  
-	        if(namco_cnt > 2) return 0xff;
+          retval = map71[namco_cnt];
+        } 
+        else {
+          static unsigned char prev_mask = 0;
+          static unsigned char fire_pending = 0;
+
+          // byte 0 is credit in BCD, byte 1 and 2: ...FLURD 
+          unsigned char mapb1[] = { (unsigned char)(16*(credit/10) + credit % 10), 0b11111111, 0b11111111 };
+
+          // get a mask of currently pressed keys
+          unsigned char keymask = input->buttons_get();
           
-	        retval = mapb1[namco_cnt];
-	        prev_mask = keymask;
-	      }
-	      namco_cnt++;
+          // report directions directly
+          if(keymask & BUTTON_LEFT)  mapb1[1] &= ~0x08;
+          if(keymask & BUTTON_UP)    mapb1[1] &= ~0x04;
+          if(keymask & BUTTON_RIGHT) mapb1[1] &= ~0x02;
+          if(keymask & BUTTON_DOWN)  mapb1[1] &= ~0x01;
+          
+          if ((keymask & BUTTON_FIRE) && !(prev_mask & BUTTON_FIRE)) {
+            fire_pending = 1;
+          }
+          if (fire_pending && namco_cnt == 1) {
+            mapb1[1] &= ~0x10;
+            fire_pending = 0;
+          }
+          
+          // 51xx leaves credit mode when user presses start? Nope ...
+          if((keymask & BUTTON_START) && !(prev_mask & BUTTON_START) && credit)
+            credit -= 1;
+          
+          if((keymask & BUTTON_COIN) && !(prev_mask & BUTTON_COIN) && (credit < 99))
+            credit += 1;
+          
+          if(namco_cnt > 2) return 0xff;
+          
+          retval = mapb1[namco_cnt];
+          prev_mask = keymask;
+        }
+        namco_cnt++;
       }
       return retval;
     }
@@ -113,26 +113,26 @@ void galaga::wrZ80(unsigned short Addr, unsigned char Value) {
       
     } else {            // 7000
       if(cs_ctrl & 1) {
-	      if(coincredMode) {
-	        coincredMode--;
-	        return;
-	      }
-	      switch(Value) {
-	        case 1:
-	          // main cpu sets coin and cred mode for both players (four bytes)
-	          coincredMode = 4;
-	          break;
-	        case 2:
-	          credit_mode = 1;
-	            break;        
-	        case 3: // disable joystick remapping
-	        case 4: // enable joystick remapping
-	          break;          
-	        case 5:
-	          credit_mode = 0;
-	          break;
-	      }
-	      namco_cnt++;
+        if(coincredMode) {
+          coincredMode--;
+          return;
+        }
+        switch(Value) {
+          case 1:
+            // main cpu sets coin and cred mode for both players (four bytes)
+            coincredMode = 4;
+            break;
+          case 2:
+            credit_mode = 1;
+            break;        
+          case 3: // disable joystick remapping
+          case 4: // enable joystick remapping
+            break;          
+          case 5:
+            credit_mode = 0;
+            break;
+        }
+        namco_cnt++;
       }
     }    
   }
@@ -154,16 +154,16 @@ void galaga::wrZ80(unsigned short Addr, unsigned char Value) {
       credit_mode = 0;   // this also resets the 51xx
       
       if(sub_cpu_reset) {
-	      current_cpu = 1; ResetZ80(&cpu[1]);
-	      current_cpu = 2; ResetZ80(&cpu[2]);
-      }	
+        current_cpu = 1; ResetZ80(&cpu[1]);
+        current_cpu = 2; ResetZ80(&cpu[2]);
+      }        
     }
     return;
   }
   
   if(Value == 0 && Addr == 0x8210) { // gg1-4.s L1725
     game_started = 1;
-    printf("game_started\n");
+    //printf("game_started\n");
   }
   
   if((Addr & 0xfff0) == 0xa000) {
@@ -176,7 +176,7 @@ void galaga::wrZ80(unsigned short Addr, unsigned char Value) {
   // inject a fixed score to be able to check hiscore routines easily
   if(game_started && (Addr == 0x8000 + 1020) && (memory[1017] == 0) && (memory[1018] == 0x24)) {
     // start with score = 23450
-      memory[1020] = 2; memory[1019] = 3; memory[1018] = 4; memory[1017] = 5; 
+    memory[1020] = 2; memory[1019] = 3; memory[1018] = 4; memory[1017] = 5; 
   }     
 #endif
 }
@@ -226,7 +226,7 @@ void galaga::run_frame(void) {
 }
 
 void galaga::prepare_frame(void) {
-  // Do all the preparations to render a screen.
+// Do all the preparations to render a screen.
 
 #ifdef LED_PIN
   led_state = 0;
@@ -245,55 +245,55 @@ void galaga::prepare_frame(void) {
       spr.flags = sprite_base_ptr[0x1b80];
       spr.x = sprite_base_ptr[0x1380] - 16;
       spr.y = sprite_base_ptr[0x1380 + 1] +
-	      0x100*(sprite_base_ptr[0x1b80 + 1] & 1) - 40;
+              0x100*(sprite_base_ptr[0x1b80 + 1] & 1) - 40;
 
       if((spr.code < 128) &&
-    	  (spr.y > -16) && (spr.y < 288) &&
-	      (spr.x > -16) && (spr.x < 224)) {      
+         (spr.y > -16) && (spr.y < 288) &&
+         (spr.x > -16) && (spr.x < 224)) {      
 
 #ifdef LED_PIN
         check_galaga_sprite(&spr);
 #endif
 
-    	  // save sprite in list of active sprites
-	      sprite[active_sprites] = spr;
-      	// for horizontally doubled sprites, this one becomes the code + 2 part
-	      if(spr.flags & 0x08) sprite[active_sprites].code += 2;	
-	      active_sprites++;
+        // save sprite in list of active sprites
+        sprite[active_sprites] = spr;
+        // for horizontally doubled sprites, this one becomes the code + 2 part
+        if(spr.flags & 0x08) sprite[active_sprites].code += 2;        
+        active_sprites++;
       }
 
       // handle horizontally doubled sprites
       if((spr.flags & 0x08) &&
-    	   (spr.y > -16) && (spr.y < 288) &&
-    	   ((spr.x+16) >= -16) && ((spr.x+16) < 224)) {
-	      // place a copy right to the current one
-	      sprite[active_sprites] = spr;
-	      sprite[active_sprites].x += 16;
-	      active_sprites++;
+         (spr.y > -16) && (spr.y < 288) &&
+         ((spr.x+16) >= -16) && ((spr.x+16) < 224)) {
+        // place a copy right to the current one
+        sprite[active_sprites] = spr;
+        sprite[active_sprites].x += 16;
+        active_sprites++;
       }
 
       // handle vertically doubled sprites
       // (these don't seem to happen in galaga)
       if((spr.flags & 0x04) &&
-	       ((spr.y+16) > -16) && ((spr.y+16) < 288) && 
-	      (spr.x > -16) && (spr.x < 224)) {      
-	      // place a copy below the current one
-	      sprite[active_sprites] = spr;
-	      sprite[active_sprites].code += 3;
-	      sprite[active_sprites].y += 16;
-	      active_sprites++;
+         ((spr.y+16) > -16) && ((spr.y+16) < 288) && 
+         (spr.x > -16) && (spr.x < 224)) {      
+        // place a copy below the current one
+        sprite[active_sprites] = spr;
+        sprite[active_sprites].code += 3;
+        sprite[active_sprites].y += 16;
+        active_sprites++;
       }
-	
+        
       // handle in both directions doubled sprites
       if(((spr.flags & 0x0c) == 0x0c) &&
-	       ((spr.y+16) > -16) && ((spr.y+16) < 288) &&
-	       ((spr.x+16) > -16) && ((spr.x+16) < 224)) {
-	      // place a copy right and below the current one
-	      sprite[active_sprites] = spr;
-	      sprite[active_sprites].code += 1;
-	      sprite[active_sprites].x += 16;
-	      sprite[active_sprites].y += 16;
-	      active_sprites++;
+         ((spr.y+16) > -16) && ((spr.y+16) < 288) &&
+         ((spr.x+16) > -16) && ((spr.x+16) < 224)) {
+        // place a copy right and below the current one
+        sprite[active_sprites] = spr;
+        sprite[active_sprites].code += 1;
+        sprite[active_sprites].x += 16;
+        sprite[active_sprites].y += 16;
+        active_sprites++;
       }
     }
   }
@@ -314,11 +314,11 @@ void galaga::check_galaga_sprite(struct sprite_S *spr) {
       // printf("SHIP %d/%d @ %d %d\n", spr->code, spr->color, spr->x, spr->y);
 
       if(spr->y == 257 && spr->code == 6)
-      	led_state = 1;    // normal fighter
-	    
+              led_state = 1;    // normal fighter
+            
       // color 9 is regular ship color, color 7 is red captured one
       if(spr->color == 7)
-      	led_state = 2;    // captured fighter
+              led_state = 2;    // captured fighter
     }
 
     // check for exploding enemies
@@ -328,8 +328,8 @@ void galaga::check_galaga_sprite(struct sprite_S *spr) {
     }
     
     // check for score marks
-    //	  if(spr->code >= 0x34 && spr->code <= 0x3a)
-    //	    printf("SCORE %d @ %d/%d\n", spr->code-0x34, spr->x, spr->y);
+    // if(spr->code >= 0x34 && spr->code <= 0x3a)
+    //   printf("SCORE %d @ %d/%d\n", spr->code-0x34, spr->x, spr->y);
   }
 }
 
@@ -372,7 +372,7 @@ void galaga::blit_sprite(short row, unsigned char s) {
   // create mask for sprites that clip left or right
   unsigned long mask = 0xffffffff;
   if(sprite[s].x < 0)      mask <<= -2*sprite[s].x;
-  if(sprite[s].x > 224-16) mask >>= (2*(sprite[s].x-(224-16)));		
+  if(sprite[s].x > 224-16) mask >>= (2*(sprite[s].x-(224-16)));                
 
   short y_offset = sprite[s].y - 8*row;
 
--- source/src/machines/galaga/galaga.h
+++ source/src/machines/galaga/galaga.h
@@ -19,55 +19,55 @@
 class galaga : public machineBase
 {
 public:
-	galaga() { }
-	~galaga() { }
+  galaga() { }
+  ~galaga() { }
 
-	signed char machineType() override { return MCH_GALAGA; } 
-	
-	unsigned char rdZ80(unsigned short Addr) override;
-	void wrZ80(unsigned short Addr, unsigned char Value) override;
-	unsigned char opZ80(unsigned short Addr) override;
-	
-	void run_frame(void) override;
-	void prepare_frame(void) override;
-	void render_row(short row) override;
+  signed char machineType() override { return MCH_GALAGA; } 
+  
+  unsigned char rdZ80(unsigned short Addr) override;
+  void wrZ80(unsigned short Addr, unsigned char Value) override;
+  unsigned char opZ80(unsigned short Addr) override;
+  
+  void run_frame(void) override;
+  void prepare_frame(void) override;
+  void render_row(short row) override;
 
-	const signed char *waveRom(unsigned char value) override;
-	const unsigned short *logo(void) override;
-	bool hasNamcoAudio() override { return true; }
+  const signed char *waveRom(unsigned char value) override;
+  const unsigned short *logo(void) override;
+  bool hasNamcoAudio() override { return true; }
 
-	// the ship explosion sound is stored as a digi sample.
-	// All other sounds are generated on the fly via the
-	// original wave tables
-	unsigned short snd_boom_cnt = 0;
-	const signed char *snd_boom_ptr = NULL;
+  // the ship explosion sound is stored as a digi sample.
+  // All other sounds are generated on the fly via the
+  // original wave tables
+  unsigned short snd_boom_cnt = 0;
+  const signed char *snd_boom_ptr = NULL;
 #ifdef LED_PIN
-	void menuLeds(CRGB *leds) override;
-	void gameLeds(CRGB *leds) override;
+  void menuLeds(CRGB *leds) override;
+  void gameLeds(CRGB *leds) override;
 #endif
 
-protected:	
-	void blit_tile(short row, char col) override;
-	void blit_sprite(short row, unsigned char s) override;
+protected:  
+  void blit_tile(short row, char col) override;
+  void blit_sprite(short row, unsigned char s) override;
 private:
-	void render_stars_set(short row, const struct galaga_star *set);
-	void trigger_sound_explosion(void);
-	void check_galaga_sprite(struct sprite_S *spr);
+  void render_stars_set(short row, const struct galaga_star *set);
+  void trigger_sound_explosion(void);
+  void check_galaga_sprite(struct sprite_S *spr);
 
-    unsigned char led_state = 0;       // state set by game (usually video driver)
+  unsigned char led_state = 0;       // state set by game (usually video driver)
 #ifdef LED_PIN
-	const CRGB menu_leds[7] = { LED_RED, LED_BLUE, LED_WHITE, LED_WHITE, LED_WHITE, LED_BLUE, LED_RED };
+  const CRGB menu_leds[7] = { LED_RED, LED_BLUE, LED_WHITE, LED_WHITE, LED_WHITE, LED_BLUE, LED_RED };
 #endif
-	unsigned char stars_scroll_y = 0;
-	unsigned char credit = 0;
-	char credit_mode = 0;
-	int namco_cnt = 0;
-	int namco_busy = 0;
-	unsigned char cs_ctrl = 0;
-	int nmi_cnt = 0;
-	int coincredMode = 0;   
-	unsigned char starcontrol = 0;
-	char sub_cpu_reset = 1;
+  unsigned char stars_scroll_y = 0;
+  unsigned char credit = 0;
+  char credit_mode = 0;
+  int namco_cnt = 0;
+  int namco_busy = 0;
+  unsigned char cs_ctrl = 0;
+  int nmi_cnt = 0;
+  int coincredMode = 0;   
+  unsigned char starcontrol = 0;
+  char sub_cpu_reset = 1;
 };
 
 #endif
